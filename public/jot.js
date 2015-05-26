e2ec2d7642ef465ad3c505d0cd3ac0b6e94eeacf
Jonathan Carter <borkabrak@gmail.com>
2015-05-26 08:10:35 -0500

    Form properly submits a PUT

--- public/jot.js
+++ public/jot.js
@@ -1,4 +1,25 @@
+function notify(message){
+    $("#notify").html(message);
+};
+
 $(function(){
     // Attach UI events
+    $("form#create").on('submit', function(event){
+        $.ajax({
+            url: "/create",
+            method: "put",
+            data: {
+                body: $(event.target.body).val()
+            },
 
+            success: function(response, statustext, xhr){
+                notify(response);
+            },
+
+            error: function(xhr, statustext, error){
+                notify(error);
+            }
+        });
+        return false;
+    });
 });
